f4c8b22512dc608c64ca6acd459c8a8e61bf1be9
Vitor Markis <vitormarkis2369@gmail.com>
2023-02-01 05:59:48 -0300

    add states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { Container } from './styles';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [country, setCountry] = useState('');
+  const [cep, setCep] = useState('');
+  const [stage, setStage] = useState(0);
+
+  function resetFields() {
+    setName('');
+    setAge('');
+    setCountry('');
+    setCep('');
+  }
+
+  function handleActionButton() {
+    switch (stage) {
+      case 0:
+        setStage(stage => stage + 1);
+        
+        break;
+      case 1:
+        console.log({
+          name,
+          age,
+          country,
+          cep
+        })
+        resetFields();
+        setStage(0)
+      default:
+        break;
+    }
+  }
+
   return (
     <Container>
-      <i>x</i>
+      {/* <i>x</i> */}
       <header>
         <h3>
           Seja <strong>bem-vindo</strong>
@@ -12,99 +46,40 @@ function App() {
       </header>
       <div className="frame-container">
         <div className="frame">
-          <div className="one">
-            <input type="text" placeholder="Nome..." />
-            <input type="text" placeholder="Idade..." />
+          <div className={`one ${stage === 1 ? 'second-stage' : ''}`}>
+            <input
+              value={name}
+              onChange={e => setName(e.target.value)}
+              type="text"
+              placeholder="Nome..."
+            />
+            <input
+              value={age}
+              onChange={e => setAge(e.target.value)}
+              type="text"
+              placeholder="Idade..."
+            />
           </div>
 
-          <div className="two">
-            <input type="text" placeholder="País..." />
-            <input type="text" placeholder="CEP..." />
+          <div className={`two ${stage === 1 ? 'second-stage' : ''}`}>
+            <input
+              value={country}
+              onChange={e => setCountry(e.target.value)}
+              type="text"
+              placeholder="País..."
+            />
+            <input
+              value={cep}
+              onChange={e => setCep(e.target.value)}
+              type="text"
+              placeholder="CEP..."
+            />
           </div>
         </div>
       </div>
-      <button>Enviar</button>
+      <button onClick={handleActionButton}>Enviar</button>
     </Container>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  position: relative;
-  width: 480px;
-  margin-top: 20rem;
-  margin-inline: auto;
-  padding: 5rem;
-  background-color: #071013;
-  color: white;
-  line-height: 1;
-
-  i {
-    font-family: Calibri;
-    position: absolute;
-    line-height: 1;
-    cursor: pointer;
-    top: 0;
-    right: 0;
-    padding: 1rem 4rem;
-    font-style: normal;
-
-    :hover {
-      background-color: #ff0035;
-    }
-  }
-  header {
-    margin-bottom: 3rem;
-    strong {
-      color: #ffb20f;
-      font-weight: 800;
-    }
-    h3 {
-      font-weight: 200;
-      margin-bottom: 1rem;
-    }
-    dd {
-      font-size: 3rem;
-      font-weight: 100;
-      color: #757575;
-    }
-  }
-
-  .frame-container {
-    .frame {
-      overflow-x: hidden;
-      width: 100%;
-      display: flex;
-      gap: 5rem;
-
-      justify-content: flex-start;
-
-      > div {
-        display: flex;
-        flex-direction: column;
-        gap: 5rem;
-        flex: 1 0 100%;
-        transition: transform 1s ease-out;
-        /* transform: translateX(calc(-100% - 5rem)); */
-      }
-    }
-  }
-
-  input {
-    background-color: #303030;
-    color: #fff;
-    padding: 4rem;
-    width: 100%;
-    border: 1px solid #757575;
-  }
-
-  button {
-    color: #fff;
-    margin-top: 5rem;
-    cursor: pointer;
-    background-color: #ff6700;
-    width: 100%;
-    padding: 4rem;
-  }
-`;
