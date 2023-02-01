74fce0f44eff827ac7d16aac2432c4fce3819016
Vitor Markis <vitormarkis2369@gmail.com>
2023-02-01 04:53:28 -0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import styled from 'styled-components';
 function App() {
   return (
     <Container>
-      <div className="frame">
-        <div className="one">
-          <input type="text" placeholder="Nome..." />
-          <input type="text" placeholder="Idade..." />
-        </div>
+      <i>x</i>
+      <header>
+        <h3>
+          Seja <strong>bem-vindo</strong>
+        </h3>
+        <dd>Faça seu registro para prosseguir</dd>
+      </header>
+      <div className="frame-container">
+        <div className="frame">
+          <div className="one">
+            <input type="text" placeholder="Nome..." />
+            <input type="text" placeholder="Idade..." />
+          </div>
 
-        <div className="two">
-          <input type="text" placeholder="País..." />
-          <input type="text" placeholder="CEP..." />
+          <div className="two">
+            <input type="text" placeholder="País..." />
+            <input type="text" placeholder="CEP..." />
+          </div>
         </div>
       </div>
       <button>Enviar</button>
@@ -22,23 +31,80 @@ function App() {
 export default App;
 
 const Container = styled.div`
-  margin-top: 20rem;
-  background-color: #aafc87;
-  padding: 5rem;
-  margin-inline: auto;
+  position: relative;
   width: 480px;
+  margin-top: 20rem;
+  margin-inline: auto;
+  padding: 5rem;
+  background-color: #071013;
+  color: white;
+  line-height: 1;
+
+  i {
+    font-family: Calibri;
+    position: absolute;
+    line-height: 1;
+    cursor: pointer;
+    top: 0;
+    right: 0;
+    padding: 1rem 4rem;
+    font-style: normal;
+
+    :hover {
+      background-color: #ff0035;
+    }
+  }
+  header {
+    margin-bottom: 3rem;
+    strong {
+      color: #ffb20f;
+      font-weight: 800;
+    }
+    h3 {
+      font-weight: 200;
+      margin-bottom: 1rem;
+    }
+    dd {
+      font-size: 3rem;
+      font-weight: 100;
+      color: #757575;
+    }
+  }
+
+  .frame-container {
+    .frame {
+      overflow-x: hidden;
+      width: 100%;
+      display: flex;
+      gap: 5rem;
+
+      justify-content: flex-start;
+
+      > div {
+        display: flex;
+        flex-direction: column;
+        gap: 5rem;
+        flex: 1 0 100%;
+        transition: transform 1s ease-out;
+        /* transform: translateX(calc(-100% - 5rem)); */
+      }
+    }
+  }
 
   input {
+    background-color: #303030;
+    color: #fff;
+    padding: 4rem;
     width: 100%;
+    border: 1px solid #757575;
   }
 
-  .frame {
-    display: flex;
-    justify-content: start;
-  }
-
-  .frame > div {
-    flex: 1 0 100%;
-    transition: transform .7s ease-in-out;
+  button {
+    color: #fff;
+    margin-top: 5rem;
+    cursor: pointer;
+    background-color: #ff6700;
+    width: 100%;
+    padding: 4rem;
   }
 `;
